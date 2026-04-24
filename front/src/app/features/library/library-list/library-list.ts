dd530fe2639f720d083bf2cff97adbaaddc1d89f
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-04-24 16:11:41 +0200

    logout from the library

--- front/src/app/features/library/library-list/library-list.ts
+++ front/src/app/features/library/library-list/library-list.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-library-list',
   templateUrl: './library-list.html',
 })
-export class LibraryList {}
+export class LibraryList {
+  router = inject(Router);
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+  }
+}
