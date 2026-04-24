c5d0e7ddca4f4d95544f90deeb2174f484e3e27e
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-04-24 12:01:56 +0200

    example routes (zero connections to real stuff as of right now)

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'library',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login')
+    .then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register')
+    .then(m => m.Register)
+  },
+  {
+    path: 'library',
+    canActivate: [authGuard],
+    loadComponent: () => import('./features/library/library-list/library-list')
+    .then(m => m.LibraryList)
+  },
+];
