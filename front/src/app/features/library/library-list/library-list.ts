79872301afa188384b32a403a136073e9c5ad546
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-06-01 14:34:15 +0200

    lib-list preliminary

---
+++ front/src/app/features/library/library-list/library-list.ts
@@ -0,0 +1,43 @@
+import { Component, computed, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { BehaviorSubject, map, switchMap } from 'rxjs';
+
+import { UserService } from '../../../core/user/user.service';
+import { GameService } from '../../../core/game/game.service';
+import { CompletionStatus } from '../../../shared/models/completion-status';
+
+import { LibraryListMember } from '../library-list-member/library-list-member';
+import { NewLibraryListMember } from '../new-library-list-member/new-library-list-member';
+
+@Component({
+  imports: [LibraryListMember, NewLibraryListMember],
+  selector: 'app-library-list',
+  templateUrl: './library-list.html',
+  styleUrl: './library-list.css',
+})
+export class LibraryList {
+  router = inject(Router);
+
+  userService = inject(UserService);
+  userinfo = toSignal(this.userService.me());
+
+  gameService = inject(GameService);
+  private refresh$ = new BehaviorSubject<void>(undefined);
+
+  libraryEntries = toSignal(
+    this.refresh$.pipe(
+      switchMap(() => this.gameService.getLibrary()),
+      map(entries => new Map(entries.map(e => [e.id, { status: e.status as CompletionStatus, rating: e.rating }])))
+    )
+  );
+
+  refreshLibrary() {
+    this.refresh$.next();
+  }
+
+  isLibraryEmpty() {
+    const l = this.libraryEntries();
+    return l === undefined || Object.keys(l).length === 0;
+  }
+}
